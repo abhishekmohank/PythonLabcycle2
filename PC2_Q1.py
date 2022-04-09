686b5838c9fe6a848231317db72e2e2ba6fd9258
Abhishek <100741799+abhishekmohank@users.noreply.github.com>
2022-04-09 09:53:48 +0530

    update

--- PC2_Q1.py
+++ PC2_Q1.py
@@ -1,10 +1,4 @@
 """
-**1. Suppose a newly born pair of rabbits, one male and one female, are
-put in a field. Rabbits can mate at the age of one month so that at the
-end of its second month, a female has produced another pair of
-rabbits. Suppose that our rabbits never die and that the female always
-produces one new pair every month from the second month.
-
 Develop a program to show a table containing the number of pairs of
 rabbits in the first N months.**
 """

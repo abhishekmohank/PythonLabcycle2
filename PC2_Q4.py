195aba192431b4f5c3ca21251b266638242a7c05
Abhishek <100741799+abhishekmohank@users.noreply.github.com>
2022-04-17 13:42:55 +0530

    fuction added

--- PC2_Q4.py
+++ PC2_Q4.py
@@ -11,86 +11,54 @@ import random
 
 class Box:
   def __init__(self,*args):
-    if len(args) == 1:
+    #if all arguements are same,cube prism is initialised.
+    if (args[0]==args[1]==args[2]):
       self.__length = args[0]
       self.__breadth = args[0]
       self.__height = args[0]
-    
-    elif len(args) == 2:
-      self.__length = args[0]
-      self.__breadth = args[0]
-      self.__height = args[1]
-    
+    #if any two arguements are same, the square prism is initialised
+    elif (args[0]==args[1] or args[1]==args[2] or args[0]==args[2]):
+      if (args[0]==args[1]):
+        self.__length = args[0]
+        self.__breadth = args[0]
+        self.__height = args[1]
+      elif (args[1]==args[2]):
+        self.__length = args[1]
+        self.__breadth = args[1]
+        self.__height = args[0]
+      elif (args[0]==args[2]):
+        self.__length = args[0]
+        self.__breadth = args[0]
+        self.__height = args[1]       
+    #if three arguements are different , rectangular prism is initialised.
     else:
       self.__length = args[0]
       self.__breadth = args[1]
       self.__height = args[2]
 
+  #funtion to calculate area
   def calc_area(self):
     self.__area = self.__length*self.__breadth
     return self.__area
-  
+  #funtion to calculate volume
   def calc_volume(self):
     self.__volume = self.__length*self.__breadth*self.__height
     return self.__volume
-  
+  #funtion to print area
   def display_area(self):
     print("The area is ",self.__area)
-  
+  #funtion to print volume
   def display_volume(self):
     print("The volume is ",self.__volume,"\n")
 
-ratio = []
 
-cube_dimension = []
-cube_dimension.append(random.randint(1,1000))
-print("Cube Prism - Dimension" , end = " ")
-print(cube_dimension)
-
-cube_prism = Box(cube_dimension[0])
-area1=cube_prism.calc_area()
-volume1=cube_prism.calc_volume()
-cube_prism.display_area()
-cube_prism.display_volume()
-
-ratio.append((volume1/area1))
-
-square_dimension = []
-for i in range(2):
-  square_dimension.append(random.randint(1,1000))
-print("Square Prism - Dimension" , end = " ")
-print(square_dimension)
-
-square_prism = Box(square_dimension[0],square_dimension[1])
-area2=square_prism.calc_area()
-volume2=square_prism.calc_volume()
-square_prism.display_area()
-square_prism.display_volume()
-
-ratio.append((volume2/area2))
-
-rectangular_dimension = []
-for i in range(3):
-  rectangular_dimension.append(random.randint(1,1000))
-
-print("Rectangular Prism - Dimension" , end = " ")
-print(rectangular_dimension)
-rectangular_prism = Box(rectangular_dimension[0],rectangular_dimension[1],rectangular_dimension[2])
-area3 = rectangular_prism.calc_area()
-volume3 = rectangular_prism.calc_volume()
-rectangular_prism.display_area()
-rectangular_prism.display_volume()
-
-ratio.append((volume3/area3))
-
+N = 10
+box = [Box(random.randint(1,1000),random.randint(1,1000),random.randint(1,1000)) for i in range(N)]
+area = [i.calc_area() for i in box]
+volume = [i.calc_volume() for i in box]
+ratio = [x//y for x,y in zip(volume,area)]
 index = ratio.index(max(ratio))
-
-if index == 0:
-  print("Cube Prism with Maximum Volume : Area - ", end=" ")
-  print(cube_dimension)
-elif index == 1:
-  print("Square Prism with Maximum Volume : Area - " , end=" ")
-  print(square_dimension)
-else:
-  print("Rectangular Prism with Maximum Volume : Area - ", end=" ")
-  print(rectangular_dimension)
+print("Box with Maximum Volume:Area ratio")
+print("Area = ",area[index])
+print("Volume = ",volume[index])
+print("Ratio = ",max(ratio))

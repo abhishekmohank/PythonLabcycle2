1f51b659e187c95c7ba1a96dae5f535f59f1d8d1
Abhishek <100741799+abhishekmohank@users.noreply.github.com>
2022-04-17 13:40:17 +0530

    fuction added

--- PC2_Q1.py
+++ PC2_Q1.py
@@ -3,14 +3,18 @@ Develop a program to show a table containing the number of pairs of
 rabbits in the first N months.**
 """
 
-n = int(input("Enter the month upto which you want the table : "))
-#adding the first no of pairs of rabbit to the list
-rabbit_pair = [1,1]
-print("\nMonths\t\tNo of Pairs of Rabbit")
-#loop used to iterate upto n-months
-for i in range(0,n):
-  #printing the month
-  print(i+1,end="\t\t")
-  #printing the pair of rabbits
-  print(rabbit_pair[i])
-  rabbit_pair.append(rabbit_pair[i]+rabbit_pair[i+1])
+#function to print rabbit pairs.
+def printRabbitPairs(n):
+  #adding the first no of pairs of rabbit to the list
+  rabbit_pair = [1,1]
+  print("\nMonths\t\tNo of Pairs of Rabbit")
+  #loop used to iterate upto n-months
+  for i in range(0,n):
+    #printing the month
+    print(i+1,end="\t\t")
+    #printing the pair of rabbits
+    print(rabbit_pair[i])
+    rabbit_pair.append(rabbit_pair[i]+rabbit_pair[i+1])
+
+noOfMonths = int(input("Enter the month upto which you want the table : "))
+printRabbitPairs(noOfMonths)
